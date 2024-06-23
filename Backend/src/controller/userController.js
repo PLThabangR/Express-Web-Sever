a7bf21bc8b67465b5009ff4dad71fbba713497bd
PLThabangR <rakgoropthabang@gmail.com>
2024-06-23 17:01:38 +0200

    Hashing user password using bcrypt

--- Backend/src/controller/userController.js
+++ Backend/src/controller/userController.js
@@ -1,4 +1,5 @@
 import { UserModal } from "../models/userSchema.js";
+import bcrypt from "bcrypt";
 
 //Register new user
 export const register = async(req,res,next) =>{
@@ -11,9 +12,18 @@ export const register = async(req,res,next) =>{
             message:"Please fill the required form fields"
         })) 
     }
+    //Check if user already exists
+    const isUser = await UserModal.findOne({email});
+    if(isUser){
+        return next(res.status(400).json({
+            success:false,
+            message:"User already Exists!!"
+        }))
+    }
+    //hash the password then pass the password as object
+    const hashedPassword = await bcrypt.hash(password,10)
     //Save user then
-
-    const user = await UserModal.create({name,email,phone,password});
+    const user = await UserModal.create({name,email,phone,password:hashedPassword});
 
     res.status(201).json({
     success:true,
