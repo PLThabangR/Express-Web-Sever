b26556f40780c47be43c400a64fd99e98c42050e
PLThabangR <rakgoropthabang@gmail.com>
2024-06-23 17:11:32 +0200

    user login controller implemented

--- Backend/src/controller/userController.js
+++ Backend/src/controller/userController.js
@@ -31,3 +31,40 @@ export const register = async(req,res,next) =>{
     user
     })
 }
+
+//Login user
+export const login=async(req,res,next)=>{
+    const {email,password} = req.body
+
+    if(!email || !password){
+        return next(res.status(400).json({
+            success:false,
+            message:"Please fill the required form fields"
+        })) 
+    } 
+
+    //Check if user exits
+    const user = await UserModal.findOne({email});
+    if(!user){
+        return next(res.status(400).json({
+            success:false,
+            message:"Invalid password or email"
+        }))
+    }
+
+    //Compare password with hashed
+    const isPasswordMatched = await bcrypt.compare(password,user.password);
+    if(!isPasswordMatched){
+        return next(res.status(400).json({
+            success:false,
+            message:"Invalid password or email"
+        }))
+    }
+
+    //if password and email are okay
+    res.status(200).json({
+        success:true,
+        message:"USer Logged in"
+    })
+
+}
