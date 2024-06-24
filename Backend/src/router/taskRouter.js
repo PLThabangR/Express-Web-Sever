97b644aad619b7a5a8cc452afd1fabc8e8bf27b6
PLThabangR <rakgoropothabang@gmail.com>
2024-06-24 16:23:15 +0200

    Completed the update function and Crud operation

--- Backend/src/router/taskRouter.js
+++ Backend/src/router/taskRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTask, deleteTask, getMyTasks } from "../controller/todoController.js";
+import { createTask, deleteTask, getMyTasks, updateTask } from "../controller/todoController.js";
 import { isAuthenticated } from "../middleware/auth.js";
 
 
@@ -10,6 +10,7 @@ const router = express.Router();
 router.post("/create",isAuthenticated,createTask);
 router.get("/tasks",isAuthenticated,getMyTasks);
 router.delete("/delete/:id",isAuthenticated,deleteTask);
+router.put("/update/:id",isAuthenticated,updateTask)
 
 
 export {router as taskRouter}
